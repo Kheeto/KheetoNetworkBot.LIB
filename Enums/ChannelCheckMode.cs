84a51dc42be2b52bd41c4e6e8e308315868714a5
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-28 10:18:24 +0200

    Create ChannelCheckMode.cs

---
+++ Enums/ChannelCheckMode.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KheetoNetworkBot.LIB.Enums
+{
+    public enum ChannelCheckMode
+    {
+        Any,
+        None,
+        MineOrParentAny
+    }
+}
